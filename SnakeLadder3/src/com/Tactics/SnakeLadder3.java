60309bda169a7bda154a2f7d28925f909df38b45
adityak-bit <adityajsr1998@gmail.com>
2020-09-25 19:41:26 +0530

    commit UC3

--- SnakeLadder3/src/com/Tactics/SnakeLadder3.java
+++ SnakeLadder3/src/com/Tactics/SnakeLadder3.java
@@ -1,12 +1,12 @@
 package com.Tactics;
 
+import java.util.Random;
+
 public class SnakeLadder3 {
 	public static void main(String[] args) {
-		int playerCount = 1;
-		int startPosition = 0;
-		System.out.println("Welcome to Snake & Ladder Game");
-		System.out.println("Game played with " + playerCount + " player(s) at start Position " + startPosition);
-
+		Random r = new Random();
+		int DiceValue = 1 + r.nextInt(6);
+		System.out.println(+DiceValue);
 	}
 
 }
