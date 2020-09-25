f079403a485958d24d461e897c01aece44e92e22
adityak-bit <adityajsr1998@gmail.com>
2020-09-25 19:49:07 +0530

    commit UC3

--- SnakeLadder3/src/com/Tactics/SnakeLadder3.java
+++ SnakeLadder3/src/com/Tactics/SnakeLadder3.java
@@ -4,9 +4,17 @@ import java.util.Random;
 
 public class SnakeLadder3 {
 	public static void main(String[] args) {
+		int NoPlay = 0;
+		int Snake = 1;
+		int Ladder = 2;
 		Random r = new Random();
-		int DiceValue = 1 + r.nextInt(6);
-		System.out.println(+DiceValue);
+		int option = r.nextInt(3);
+		if (option == 0)
+			System.out.println("NoPlay");
+		else if (option == 1)
+			System.out.println("Snake");
+		else
+			System.out.println("Ladder");
 	}
 
 }
