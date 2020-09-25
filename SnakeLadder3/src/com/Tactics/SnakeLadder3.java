3ea1add931267ef5c1bf63bea8bc27fcdfd62244
adityak-bit <adityajsr1998@gmail.com>
2020-09-25 19:34:13 +0530

    commit UC1

---
+++ SnakeLadder3/src/com/Tactics/SnakeLadder3.java
@@ -0,0 +1,12 @@
+package com.Tactics;
+
+public class SnakeLadder3 {
+	public static void main(String[] args) {
+		int playerCount = 1;
+		int startPosition = 0;
+		System.out.println("Welcome to Snake & Ladder Game");
+		System.out.println("Game played with " + playerCount + " player(s) at start Position " + startPosition);
+
+	}
+
+}
