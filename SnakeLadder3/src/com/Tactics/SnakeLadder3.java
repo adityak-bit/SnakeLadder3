57a5c30acdf563bf6d4ecd0f635bc332fdb09c9b
adityak-bit <adityajsr1998@gmail.com>
2020-09-30 15:24:48 +0530

    [Aditya] Update : changed the naming convention

--- SnakeLadder3/src/com/Tactics/SnakeLadder3.java
+++ SnakeLadder3/src/com/Tactics/SnakeLadder3.java
@@ -4,9 +4,9 @@ import java.util.Random;
 
 public class SnakeLadder3 {
 	public static void main(String[] args) {
-		Random r = new Random();
-		int DiceValue = 1 + r.nextInt(6);
-		System.out.println(+DiceValue);
+		Random random = new Random();
+		int diceValue = 1 + random.nextInt(6);
+		System.out.println(+diceValue);
 	}
 
 }
